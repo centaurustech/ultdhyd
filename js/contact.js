144f0fe7a4498cee845dc54a552328345b384802
Pallav Bajjuri <bajjuri@viveninfomedia.com>
2015-07-31 19:43:24 +0530

    July 28th Update from Raj Janagam

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 $('#map_addresses').gMap({
-	address: "UnLtd Hyderabad, H.No.8-2-326/A/2/A, Road No.3, Banjara Hills, Hyderabad 500034. ",
+	address: "UnLtd Hyderabad, Banjara Hills",
 	zoom: 15,
 	arrowStyle: 2,
 	controls: {
